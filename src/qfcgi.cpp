09acbe61ed12aac94eb5893d0565c58a0fc560b1
Robin Doer <robin@robind.de>
2014-08-01 10:48:37 +0200

    Initial commit

---
+++ src/qfcgi.cpp
@@ -0,0 +1,42 @@
+/**
+ * This file is part of QFCgi.
+ *
+ * QFCgi is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * QFCgi is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QFCgi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QTcpServer>
+#include <QTcpSocket>
+
+#include "qfcgi.h"
+
+QFCgi::QFCgi(QObject *parent) : QObject(parent) {
+  this->server = new QTcpServer(this);
+}
+
+QFCgi::~QFCgi() {
+
+}
+
+void QFCgi::start() {
+  this->server->listen(QHostAddress::Any, 9000);
+
+  connect(this->server, SIGNAL(newConnection()), this, SLOT(onNewConnection()));
+
+  qDebug() << "FastCGI application started";
+}
+
+void QFCgi::onNewConnection() {
+  QTcpSocket *so = this->server->nextPendingConnection();
+  delete so;
+}
---
+++ src/qfcgi.h
@@ -0,0 +1,42 @@
+/**
+ * This file is part of QFCgi.
+ *
+ * QFCgi is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * QFCgi is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with QFCgi. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QFCGI_H
+#define QFCGI_H
+
+#include <QObject>
+
+class QTcpServer;
+
+class QFCgi : public QObject {
+  Q_OBJECT
+
+public:
+  QFCgi(QObject *parent = 0);
+  virtual ~QFCgi();
+
+public slots:
+  void start();
+
+private slots:
+  void onNewConnection();
+
+private:
+  QTcpServer *server;
+};
+
+#endif  /* QFCGI_H */
